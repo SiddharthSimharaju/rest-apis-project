3460541bce919508f24b4cf69e34f2b588cdcb23
Siddharth Simharaju <siddharth.simharaju@gmail.com>
2022-12-19 14:47:00 +0530

    added model changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask import Flask, jsonify
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -19,6 +20,8 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
